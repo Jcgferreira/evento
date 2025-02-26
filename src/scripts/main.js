1cfe91bac2c77b0bac2cc01d671796f6b1477576
Jcgferreira <jcgferreira1220@outlook.com>
2025-02-26 00:54:07 -0300

    inseridas 3 secoes about clubs ticket

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-console.log("Olá mundo");
+AOS.init();
+
+const dataPromo = new Date("Dec 21, 2025 23:59:59");
+const timesTempDaPromo = dataPromo.getTime();
+
+const contaHora = setInterval(function (){
+    const agora = new Date();
+    const timesTempAtual = agora.getTime();
+
+    const tempoRestante = timesTempDaPromo - timesTempAtual;
+
+    const dias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((tempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((tempoRestante % (1000 * 60)) / 1000);
+
+    document.getElementById("contador").innerHTML = "A KINGS LEAGUE INICIA EM: " + dias + " Dias " + horas + " h " + minutos + " m " + segundos + " s ";
+}, 1000)
